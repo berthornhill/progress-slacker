a88eaac578c037378d9592389ee2b5afc99b8291
berthornhill <64211468+berthornhill@users.noreply.github.com>
2021-02-01 08:42:36 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World, I am Progress Slacker"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+const mongoose = require("mongoose");
