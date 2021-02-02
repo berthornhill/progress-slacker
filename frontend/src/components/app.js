9bfb722ecdac23b6959a14f67fc6f071b35ea646
berthornhill <64211468+berthornhill@users.noreply.github.com>
2021-02-02 14:45:07 -0500

    Resolved some conflicts with main

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,19 +2,24 @@ import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
 import { Route, Redirect, withRouter } from "react-router-dom";
+import NavBarContainer from "./nav/navbar_container";
+import LoginFormContainer from "./session/login_form_container";
+import SignupFormContainer from "./session/signup_form_container";
 import MemeCanvas from "./meme_gen/meme_canvas";
 
-// import Dashboard from "./dashboard";
+import Dashboard from "./dashboard/dashboard";
 
 const App = () => (
+  // <div>dashboard</div>
   <div>
-    dashboard
-    <MemeCanvas />
+    <NavBarContainer />
+    <Switch>
+      <Route path="/" component={MemeCanvas} />
+      <Route exact path="/" component={Dashboard} />
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    </Switch>
   </div>
-
-  //   <Switch>
-  //     <Route exact path="/" component={Dashboard} />
-  //   </Switch>
 );
 
 export default App;
