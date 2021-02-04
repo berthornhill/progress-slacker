8304727b0f418c84f242b60da3c6e0fb0f9f2d5f
berthornhill <64211468+berthornhill@users.noreply.github.com>
2021-02-03 22:36:12 -0500

    got Dan loaded

--- frontend/src/components/meme_gen/meme_canvas_container.js
+++ frontend/src/components/meme_gen/meme_canvas_container.js
@@ -1,16 +1,19 @@
 import { connect } from "react-redux";
 import { fetchMeme } from "../../actions/meme_template_actions";
 import MemeCanvas from "./meme_canvas";
+import { _arrayBufferToBase64 } from "../../util/image_util";
 
 const mSTP = (state) => {
+  debugger;
   return {
-    fetchedMeme: "",
+    memes: state.entities.memes,
   };
 };
 
 const mDTP = (dispatch) => {
   return {
     fetchMeme: (id) => dispatch(fetchMeme(id)),
+    _arrayBufferToBase64,
   };
 };
 
