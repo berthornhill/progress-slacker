b269e49a4e745b7b4ef928c42cae6f3d86adb28d
berthornhill <64211468+berthornhill@users.noreply.github.com>
2021-02-03 22:53:41 -0500

    once more for comfort

--- frontend/src/reducers/meme_reducer.js
+++ frontend/src/reducers/meme_reducer.js
@@ -7,14 +7,9 @@ import {
 
 const MemesReducer = (state = {}, action) => {
   Object.freeze(state);
-<<<<<<< HEAD
-  const { meme, memes, memeId } = action;
-  debugger
-=======
   debugger;
   const { payload } = action;
 
->>>>>>> 62d7a93fae10cffb4774bd2bcc23074277310b60
   switch (action.type) {
     case FETCH_MEMES:
       return Object.assign({}, state, payload.data);
