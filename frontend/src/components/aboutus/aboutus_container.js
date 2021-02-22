62eb78f1b77b3aef957740858eaa3efa9d928125
Mike Criollo <mkcriollo@gmail.com>
2021-02-22 13:51:26 -0500

    create about us

---
+++ frontend/src/components/aboutus/aboutus_container.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import AboutUs from './aboutus'
+
+const mSTP = (state,ownProps) => {
+    return {
+
+    }
+}
+
+const mDTP = dispatch => {
+    return {
+
+    }
+}
+
+
+export default connect(mSTP,mDTP)(AboutUs)
--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -38,6 +38,35 @@ class DashBoard extends React.Component {
         </div>
       </div>
     );
+    // return (
+    //   <div className="full-page">
+    //     <div className="main-page">
+    //       <SideBarContainer />
+    //       {/* <div className="animate__animated animate__fadeInLeftBig">
+
+    //         <div>Lets</div>
+    //         <div>Talk</div>
+    //         <div>Memes</div>
+    //         <div className="squid"></div>
+    //       </div> */}
+    //       <Switch>
+    //         <Route
+    //           path="/:id"
+    //           component={MemeFeed}
+    //           featured={this.props.toDisplay}
+    //         />
+    //         <Route
+    //           path="/"
+    //           component={MemeFeed}
+    //           featured={this.props.toDisplay}
+    //         />
+    //       </Switch>
+    //     </div>
+    //     {/* <div className="memes-main-content">
+
+    //     </div> */}
+    //   </div>
+    // );
   }
 }
 
